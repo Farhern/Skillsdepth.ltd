c7b581376cb9e7082b127a9b29d7401464f26e03
farhern <farhanjp7452@gmail.com>
2023-12-12 18:22:03 +0300

    finished about page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
-import About from "./components/Container/About";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import AboutPage from "./components/Pages/AboutPage";
 import ProjectRouters from "./components/ProjectRouters";
+
 
 
 export default function App() {
@@ -14,24 +14,11 @@ export default function App() {
         <Route path="/" element={<Navbar />}>
           <Route index element={<ProjectRouters />} />
           <Route path="/aboutpage" element={<AboutPage />} />
-          <Route path="about" element={ <About /> }/>
         </Route>
+        
       </Routes>
    </BrowserRouter>
   );
-  // return (
-  //   <div className="font-Poppins bg-Solitude">
-    
-  //     <Navbar />
-  //     <Home />
-  //     <About />
-  //     <Services />
-  //     <Contact />
-  //     <Footer />
-  //     <Routers />
-  //   </div>
-    
-  // );
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/components/Pages/AboutPage.jsx
+++ src/components/Pages/AboutPage.jsx
@@ -1,10 +1,65 @@
 import React from 'react';
+import Footer from '../Container/Footer';
 
 const AboutPage = () => {
   return (
-    <div className='section' id='aboutpage'>
-      <h1>hello</h1>
+    <>
+            <div className='section' id='aboutpage'>
+      <div>
+        <h1 className='text-center font-bold'>VISION</h1>
+        <p className="text-sm text-gray-700 leading-7 max-w-[700px] mx-auto mt-2">
+        Our vision is to become the preferred training provider in Kenya and the region. We
+        aim to be the go-to institution for individuals seeking high-quality training and
+        development opportunities.
+        </p>
+        <h1 className='text-center font-bold mt-3'>MISION</h1>
+        <p className="text-sm text-gray-700 leading-7 max-w-[700px] mx-auto mt-2">
+        Our mission is to address the skills gap within the country's workforce and provide
+        solutions to real-world challenges. We achieve this through rigorous research and the
+        delivery of cutting-edge training and consultancy services. Our commitment is to
+        empower individuals and organizations with the skills and knowledge needed for
+        success.
+        </p>
+        <h1 className='text-center font-bold mt-3'>CORE VALUES</h1>
+        <p className="text-sm text-gray-700 leading-7 max-w-[700px] mx-auto mt-3">
+        At <span className='font-bold text-black'>AdvancedSkills Hub</span>, we hold the following core values in high regard:
+        </p>
+        <p className="text-sm text-gray-700 leading-7 max-w-[700px] mx-auto mt-2">
+        1. <span className='font-bold text-black'>Integrity:</span> We are committed to upholding the highest standards of integrity in all our
+             interactions. Our actions are guided by strong ethical principles.
+        </p>
+        <p className="text-sm text-gray-700 leading-7 max-w-[700px] mx-auto mt-2">
+        2. <span className='font-bold text-black'>Honesty:</span> We value honesty and transparency in our relationships with clients, students,
+             and stakeholders. We strive to be truthful and forthright in all our dealings.
+        </p>
+        <p className="text-sm text-gray-700 leading-7 max-w-[700px] mx-auto mt-2">
+        3. <span className='font-bold text-black'>Reliability:</span> We take pride in being a reliable and consistent partner. Our clients can
+             trust us to deliver on our promises and provide high-quality training and services.
+        </p>
+        <p className="text-sm text-gray-700 leading-7 max-w-[700px] mx-auto mt-2">
+        4. <span className='font-bold text-black'>Transparency:</span> Transparency is essential to our operations. We believe in open and
+             clear communication, ensuring that all stakeholders are informed about our processes
+             and decisions.
+        </p>
+        <p className="text-sm text-gray-700 leading-7 max-w-[700px] mx-auto mt-2">
+        5. <span className='font-bold text-black'>PARTNERSHIP:</span> We prioritize close collaboration with our clients to understand their unique
+             needs and co-create solutions that deliver sustainable results.
+        </p>
+        <p className="text-sm text-gray-700 leading-7 max-w-[700px] mx-auto mt-2">
+        6. <span className='font-bold text-black'>IMPACT:</span> We aim to make a measurable difference in the performance and development
+             of individuals and organizations.
+        </p>
+        <p className="text-sm text-gray-700 leading-7 max-w-[700px] mx-auto mt-2">
+        7. <span className='font-bold text-black'>EMPOWERMENT:</span> We believe in fostering a culture of self-belief, enabling individuals and
+             organizations to take ownership of their growth and achievements.
+        </p>
+      </div>
+     
     </div>
+    <Footer />
+    </>
+
+     
   )
 };
 
